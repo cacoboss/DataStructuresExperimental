495e694d02729b2586ed8fed107996b42a2a31f8
cacoboss <cacoboss@unal.edu.co>
2014-06-01 16:49:42 -0500

    Second Commit for Workshop2

--- src/co/edu/unal/ds/main/PaintingWalls.java
+++ src/co/edu/unal/ds/main/PaintingWalls.java
@@ -40,7 +40,7 @@ public class PaintingWalls {
         colSize = sc.nextInt();
         System.out.println( "Please enter the maze: " );
         //Initializing maze
-        maze = new char[ rowSize + 4 ][ colSize + 4];
+        maze = new char[ rowSize + 4 ][ colSize + 4 ];
         for(int i = 0; i < rowSize; i++ ){
             initial = sc.next();
             currentRow = initial.toCharArray();
@@ -49,6 +49,10 @@ public class PaintingWalls {
             }
         }
         System.out.println();
+    }
+    
+    public static boolean ok( int r, int c, int rowSize, int colSize ){
+        return ( r > 0 && c > 0 && r < rowSize + 3 && c < colSize + 3);
     }
     
     public static void isOkayToPaint( ){
@@ -61,17 +65,24 @@ public class PaintingWalls {
         
         //Initialing External Corridor   ///****///
         for(int i = 0; i < rowSize + 4; i++){
-            maze[ i ][ 0 ] = '.';
+            //Creating External wall of ****
+            maze[ i ][ 0 ] = '*';
+            maze[ i ][ rowSize + 3 ] = '*';
+        }
+        for(int i = 0; i < rowSize + 4; i++){
+            //Creating Internal wall of ....
             maze[ i ][ 1 ] = '.';
             maze[ i ][ rowSize + 2 ] = '.';
-            maze[ i ][ rowSize + 3 ] = '.';
         }
-        
-        for(int i = 0; i < colSize + 4; i++){
-            maze[ 0 ][ i ] = '.';
+        for(int i = 1; i < colSize + 3; i++){
+            //Creating External wall of ****
+            maze[ 0 ][ i ] = '*';
+            maze[ colSize + 3 ][ i ] = '*';
+        }
+        for(int i = 1; i < colSize + 3; i++){
+            //Creating Internal wall of ....
             maze[ 1 ][ i ] = '.';
             maze[ colSize + 2 ][ i ] = '.';
-            maze[ colSize + 3 ][ i ] = '.';
         }
    
         //Initializing the Queue
@@ -79,43 +90,45 @@ public class PaintingWalls {
         Position nbr = new Position(1,1);
         
         //Print the laberint
-        /*for(int i = 0; i < rowSize + 4; i++){
+        for(int i = 0; i < rowSize + 4; i++){
             for(int j = 0; j < colSize + 4; j++){
                 System.out.print(maze[i][j]);
             }
             System.out.println();
         }
-         System.out.println();*/
+         System.out.println();
         
         //Scan maze and the counting
         for(int i = 1; i < rowSize + 2; i++){  //row i of maze
             for(int j = 1 ; j < colSize + 2; j++){  //col j of maze
-                if(maze[i][j] == '.'){ //Posible Component
-                    maze[i][j] = '*'; //Position already checked
+                if(ok(nbr.row,nbr.col,rowSize,colSize) && maze[i][j] == '.'){ //Posible Component
+                //    maze[i][j] = '*'; //Position already checked
                 //    littersOfPaint = littersOfPaint + 1; ///****///
                     here = new Position( i , j );
                     System.out.println("Punto inicio Row:" + here.row + " y Col:" + here.col);
                     do{ //Find Walls for painting
+                        maze[here.row][here.col] = '*'; //Position already checked
                         for(int aux = 0; aux < 3 ; aux++){
                             //Check all neighbors
                             nbr.row = (here.row + offsets[aux].row);
                             nbr.col = (here.col + offsets[aux].col);
                             System.out.println("Los indices son Row:" + nbr.row + " y Col:" + nbr.col);
-                            if( maze[ nbr.row ][ nbr.col ] == '#' ){
+                            if(ok(nbr.row,nbr.col,rowSize,colSize) && maze[ nbr.row ][ nbr.col ] == '#' ){
                                 //Possible Wall for painting
                                 littersOfPaint = littersOfPaint + 1;              
                             }else {
-                                queuePaint.put(new Position(nbr.row,nbr.col));
+                                if(maze[ nbr.row ][ nbr.col ] == '.')
+                                    queuePaint.put(new Position(nbr.row,nbr.col));
                             }
                             
                             //Revisando que funcione
-                            /*for(int aux1 = 0; aux1 < rowSize + 4; aux1++){
+                            for(int aux1 = 0; aux1 < rowSize + 4; aux1++){
                                 for(int aux2 = 0; aux2 < colSize + 4; aux2++){
                                     System.out.print(maze[aux1][aux2]);
                                 }
                                 System.out.println();
                             }
-                            System.out.println();*/
+                            System.out.println();
                             
                         }
                         //Unexplored Path
